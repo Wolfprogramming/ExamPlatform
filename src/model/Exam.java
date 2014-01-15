062778614af474b13eadebe22c645e7dc5f2ae7c
apo <trashc73@gmail.com>
2014-01-15 18:15:49 +0100

    Testing master branch

---
+++ src/model/Exam.java
@@ -0,0 +1,119 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the exam database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Exam.findAll", query="SELECT e FROM Exam e")
+public class Exam implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int idExam;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date eCreationDate;
+
+	@Temporal(TemporalType.DATE)
+	private Date eDate;
+
+	@Lob
+	private String eDesc;
+
+	private Time eEndHour;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date eModifyDate;
+
+	private String eName;
+
+	private Time eStartHour;
+
+	//bi-directional many-to-one association to Teacher
+	@ManyToOne
+	@JoinColumn(name="FK_idTeacher")
+	private Teacher teacher;
+
+	public Exam() {
+	}
+
+	public int getIdExam() {
+		return this.idExam;
+	}
+
+	public void setIdExam(int idExam) {
+		this.idExam = idExam;
+	}
+
+	public Date getECreationDate() {
+		return this.eCreationDate;
+	}
+
+	public void setECreationDate(Date eCreationDate) {
+		this.eCreationDate = eCreationDate;
+	}
+
+	public Date getEDate() {
+		return this.eDate;
+	}
+
+	public void setEDate(Date eDate) {
+		this.eDate = eDate;
+	}
+
+	public String getEDesc() {
+		return this.eDesc;
+	}
+
+	public void setEDesc(String eDesc) {
+		this.eDesc = eDesc;
+	}
+
+	public Time getEEndHour() {
+		return this.eEndHour;
+	}
+
+	public void setEEndHour(Time eEndHour) {
+		this.eEndHour = eEndHour;
+	}
+
+	public Date getEModifyDate() {
+		return this.eModifyDate;
+	}
+
+	public void setEModifyDate(Date eModifyDate) {
+		this.eModifyDate = eModifyDate;
+	}
+
+	public String getEName() {
+		return this.eName;
+	}
+
+	public void setEName(String eName) {
+		this.eName = eName;
+	}
+
+	public Time getEStartHour() {
+		return this.eStartHour;
+	}
+
+	public void setEStartHour(Time eStartHour) {
+		this.eStartHour = eStartHour;
+	}
+
+	public Teacher getTeacher() {
+		return this.teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+}
---
+++ src/model/Teacher.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the teacher database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Teacher.findAll", query="SELECT t FROM Teacher t")
+public class Teacher implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int idTeacher;
+
+	private String tEmail;
+
+	private String tFirstName;
+
+	private String tLastName;
+
+	private String tPassword;
+
+	//bi-directional many-to-one association to Exam
+	@OneToMany(mappedBy="teacher")
+	private List<Exam> exams;
+
+	public Teacher() {
+	}
+
+	public int getIdTeacher() {
+		return this.idTeacher;
+	}
+
+	public void setIdTeacher(int idTeacher) {
+		this.idTeacher = idTeacher;
+	}
+
+	public String getTEmail() {
+		return this.tEmail;
+	}
+
+	public void setTEmail(String tEmail) {
+		this.tEmail = tEmail;
+	}
+
+	public String getTFirstName() {
+		return this.tFirstName;
+	}
+
+	public void setTFirstName(String tFirstName) {
+		this.tFirstName = tFirstName;
+	}
+
+	public String getTLastName() {
+		return this.tLastName;
+	}
+
+	public void setTLastName(String tLastName) {
+		this.tLastName = tLastName;
+	}
+
+	public String getTPassword() {
+		return this.tPassword;
+	}
+
+	public void setTPassword(String tPassword) {
+		this.tPassword = tPassword;
+	}
+
+	public List<Exam> getExams() {
+		return this.exams;
+	}
+
+	public void setExams(List<Exam> exams) {
+		this.exams = exams;
+	}
+
+	public Exam addExam(Exam exam) {
+		getExams().add(exam);
+		exam.setTeacher(this);
+
+		return exam;
+	}
+
+	public Exam removeExam(Exam exam) {
+		getExams().remove(exam);
+		exam.setTeacher(null);
+
+		return exam;
+	}
+
+}
