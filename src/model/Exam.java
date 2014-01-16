59198b00156844cdd58b467fdcf107aa546b47d9
alexisyvin <alexisyvin@192.168.0.102>
2014-01-16 13:48:41 +0100

    changes in entitites

--- src/model/Exam.java
+++ src/model/Exam.java
@@ -16,7 +16,7 @@ public class Exam implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idExam;
 
 	@Temporal(TemporalType.TIMESTAMP)
